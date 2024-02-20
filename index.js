b8224ac64fe99f4434f64e88fc4241de04f40770
adanguerra1990 <aj90guerra@gmail.com>
2024-02-20 15:23:52 -0400

    Ejercicio 3.9 Backend de la Agenda Telefónica, paso 9

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(express.json())
@@ -107,6 +110,7 @@ app.post('/api/persons', (request, response) => {
 
 
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server Running on Port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () =>{
+    console.log(`Server Running on Port ${PORT}`)
+})
